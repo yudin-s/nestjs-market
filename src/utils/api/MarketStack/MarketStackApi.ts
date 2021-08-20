1d5ac1ed9e8fdd02ff3cca9466042dcd0846f0b1
Admin <enfros2000@gmail.com>
2021-08-20 22:18:05 +0300

    - Fix export issue

--- src/utils/api/MarketStack/MarketStackApi.ts
+++ src/utils/api/MarketStack/MarketStackApi.ts
@@ -12,24 +12,34 @@ export class MarketStackApi {
       headers: { 'X-Custom-Header': 'foobar' },
     });
   }
-
-  async sendGetRequest(params): Promise<MarketStackResponse> {
+  /**
+   * Facade of axios data api
+   * @param params parameters that will be passed to marketstack
+   * @returns data from API
+   */
+  async sendGetRequest({ url, params }): Promise<MarketStackResponse> {
     const result = await this.axios.request<
       unknown,
       AxiosResponse<MarketStackResponse>
     >({
       method: 'GET',
+      url,
       params: {
-        access_key: process.env.API_KEYS,
+        access_key: process.env.API_KEY,
         ...params,
       },
     });
     return result.data;
   }
-
+  /**
+   * Fetch ticker information
+   * @param params parameters of request
+   * @returns information with data tickers
+   * @see https://marketstack.com/documentation
+   */
   async fetchTickerEOD(
     params: MarketStackRequest,
   ): Promise<MarketStackResponse> {
-    return this.sendGetRequest(params);
+    return this.sendGetRequest({ url: '/eod', params });
   }
 }
